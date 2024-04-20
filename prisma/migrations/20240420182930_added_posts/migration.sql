a541c3cc37434300d63d2d42bfc67fe2c1ebd02b
gsjoy24 <gour.joy24@gmail.com>
2024-04-21 00:33:08 +0600

    Add ts-node-dev as a development dependency in package.json Update schema.prisma to remove unnecessary comments and update table mapping Remove migration.sql file

---
+++ prisma/migrations/20240420182930_added_posts/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
