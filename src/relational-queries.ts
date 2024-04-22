2a598ae6b78fb82065e532e6b06cde8f63b15348
gsjoy24 <gour.joy24@gmail.com>
2024-04-22 18:49:25 +0600

    added queries

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -3,7 +3,28 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const relationalQueries = async () => {
-	console.log('Create User');
+	//! find user with id 1 and include all posts
+	// const findUser = await prisma.user.findUnique({
+	// 	where: {
+	// 		id: 1
+	// 	},
+	// 	include: {
+	// 		post: true
+	// 	}
+	// })
+
+	//! find users who have published posts
+	const publishedPosts = await prisma.user.findMany({
+		include: {
+			post: {
+				where: {
+					published: true
+				}
+			}
+		}
+	});
+
+	console.log(publishedPosts);
 };
 
 relationalQueries();
